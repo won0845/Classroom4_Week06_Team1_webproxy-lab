836ad7afeb251d1c3497151e079a0b37197be778
May Park <68367393+pkw-May@users.noreply.github.com>
2024-02-27 21:56:32 +0900

    Hw 7,10 (#5)
    * add: mp4 file
    
    * add form tag to get adder page
    
    ---------
    
    Co-authored-by: won0845 <153366472+won0845@users.noreply.github.com>

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -2,26 +2,26 @@
 
 int main(void)
 {
-  char *buf, *p;
-  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+  char *buf, *pair;
+  char content[MAXLINE];
   int n1 = 0, n2 = 0;
 
-  /* Extract the two arguments */
   if ((buf = getenv("QUERY_STRING")) != NULL)
   {
-    p = strchr(buf, '&');
-    *p = '\0';
-    strcpy(arg1, buf);
-    strcpy(arg2, p + 1);
-    n1 = atoi(arg1);
-    n2 = atoi(arg2);
+    pair = strtok(buf, "&");
+    while (pair != NULL)
+    {
+      sscanf(pair, "num1=%d", &n1);
+      sscanf(pair, "num2=%d", &n2);
+      pair = strtok(NULL, "&");
+    }
   }
 
   /* Make the response body */
   sprintf(content, "QUERY_STRING=%s", buf);
   sprintf(content, "Welcome to add.com: ");
   sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
-  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",
+  sprintf(content, "%sThe answer is\r\n<p>%d + %d = %d\r\n<p>",
           content, n1, n2, n1 + n2);
   sprintf(content, "%sThanks for visiting!\r\n", content);
 
