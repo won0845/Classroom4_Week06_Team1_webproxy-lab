f403e46a986b7619486ee9e14c76d3d5aaef5144
won0845 <tpqms775@gmail.com>
2024-02-29 01:35:01 +0900

    echo test

--- echo.c
+++ echo.c
@@ -10,6 +10,7 @@ void echo(int connfd)
     while ((n = Rio_readlineb(&rio, buf, MAXLINE)) != 0)
     {
         printf("server received %d bytes\n", (int)n);
+        printf(buf,stdout);
         Rio_writen(connfd, buf, n);
     }
 }
